460b0aeeabff498207e3185488cb33d8bf24499e
dailly101 <vladsizov2006@mail.ru>
2025-03-25 02:59:03 +0300

    SECOND

--- Sizov_LR3_project.cpp
+++ Sizov_LR3_project.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+void Existence(double a, double b, double c) {
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "Треугольник существует." << endl;
+    }
+    else {
+        cout << "Треугольник не существует." << endl;
+    }
+}
 
 int main() {
     double a, b, c;
