2aa0b5b5f8e974b34bf93f897460bf7343e24005
dailly101 <vladsizov2006@mail.ru>
2025-03-25 02:57:11 +0300

    FIRST

---
+++ Sizov_LR3_project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    double a, b, c;
+    cout << "Введите длину стороны A: ";
+    cin >> a;
+    cout << "Введите длину стороны B: ";
+    cin >> b;
+    cout << "Введите длину стороны C: ";
+    cin >> c;
+    return 0;
+}
